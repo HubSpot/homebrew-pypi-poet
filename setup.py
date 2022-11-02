fded578229b507e25626cf22f0675c84517630de
Alex Thompson <athompson@hubspot.com>
2022-11-02 15:42:27 -0400

    Put an upperbound on `pypi-simple` requirement because v1 has breaking changes

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ],
-    install_requires=['jinja2', 'pypi-simple', 'setuptools'],
+    install_requires=['jinja2', 'pypi-simple < 1', 'setuptools'],
     entry_points={'console_scripts': [
         'poet=poet:main',
         'poet_lint=poet.lint:main',
